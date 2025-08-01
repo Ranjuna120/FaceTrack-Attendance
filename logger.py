1c9c8132e68197fcbe3ba98e518a07caa6e2aa4e
Ranjuna120 <IT22236296@my.sliit.lk>
2025-08-01 09:36:30 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,11 @@
+# Centralized logger for FaceTrack-Attendance
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+
+def get_logger(name):
+    return logging.getLogger(name)
